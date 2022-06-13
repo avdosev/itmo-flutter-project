2ecdc3230305d8022a611cb0ec69680cf42ed7c1
Nikita Avdosev <avdosev1@yandex.ru>
2022-06-13 23:09:02 +0300

    template order

---
+++ lib/pages/components/select_order_template.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TemplateOrderSelector extends StatelessWidget {
+  const TemplateOrderSelector({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/create_order.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CreateOrder extends StatelessWidget {
+  const CreateOrder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Создание заказа')),
+      
+    );
+  }
+}
